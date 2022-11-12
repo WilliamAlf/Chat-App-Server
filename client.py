5dc0c556f94c3c46c8815f08cd75b1ba4979e325
WilliamA <william.alfredsson@hotmail.com>
2022-11-12 22:04:27 +0100

    server initial testing

---
+++ client.py
@@ -0,0 +1,27 @@
+import socket
+
+server_ip = "192.168.66.3"
+server_port = 55555
+
+server = (server_ip, server_port)
+
+socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+socket.bind("", 50001)
+socket.sendto("pass", server)
+
+while True:
+    data = socket.recv(1024).decode()
+    
+    if data.strip() == "success":
+        print("checked in with server, waiting")
+        break
+    
+
+ip, sport, dport = socket.recv(1024).decode()
+
+print("\nGot Peer")
+print(f"\tip: {ip}")
+print(f"\tsource port: {sport}")
+print(f"\tdest port: {dport}\n")
+
+  
---
+++ server.py
@@ -0,0 +1,27 @@
+import socket
+
+connections = []
+
+#Bug - passwrods must be successfulyl sent for program to work
+
+#Listener
+knownport = 50002
+
+socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+socket.bind(("", 55555))
+
+clients = {}
+
+while True:
+    new_password, new_addr = socket.recvfrom(1024)
+    print(f"connection from {new_addr}")
+    
+    clients[new_addr] = new_password
+    
+    socket.sendto(b"success", addr)
+    
+    for addr, password in clients.items:
+        if new_password == password:
+            socket.sendto((addr[0], addr[1], knownport), new_addr)
+            socket.sendto((new_addr[0], new_addr[1], knownport), addr)
+            
